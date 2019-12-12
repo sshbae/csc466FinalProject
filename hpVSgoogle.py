395846378bc1c41e431a4e38abd725adf28db88e
wesley <wbenica@calpoly.edu>
2019-12-12 13:53:35 -0800

    standardized column headers homeowner datasets

--- hpVSgoogle.py
+++ hpVSgoogle.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     avo_trends = clean_na(avo_trends)
     avo_toast_trends = pd.read_csv('./finalProj/trends_avocado_toast.csv', index_col='state')
     avo_toast_trends = clean_na(avo_toast_trends)
-    h_owner_rate = pd.read_csv('./finalProj/homeownership_rates_by_state.csv', header=[0, 1], index_col=0).astype(np.float)
-    h_vacancy_rate = pd.read_csv('./finalProj/homeowner_vacancy_rates.csv', header=[0, 1], index_col=0).astype(np.float)
+    h_owner_rate = pd.read_csv('finalProj/homeownership_rates_by_state.csv', header=[0, 1], index_col=0).astype(np.float)
+    h_vacancy_rate = pd.read_csv('finalProj/homeowner_vacancy_rates.csv', header=[0, 1], index_col=0).astype(np.float)
 
     avo_centroids, avo_clusters = vq.kmeans2(avo_trends, k=4)
     toast_centroids, toast_clusters = vq.kmeans2(avo_toast_trends, k=4)
