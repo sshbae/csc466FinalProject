46c70c339d13dd461e9209013e7715abc752b4b5
Sarah <sarahbae1998@gmail.com>
2019-12-12 12:08:52 -0800

    apDateRemover replaces Dates with Months and one hot encodes type

--- apVSgoogle.py
+++ apVSgoogle.py
@@ -1,1 +1,14 @@
+import scipy
+import plotly.figure_factory as ff
+import pandas as pd
+import numpy as np
 
+def main():
+    apdf = pd.read_csv("./finalProj/avoPricesCategor.csv")
+    googdf = pd.read_csv("./finalProj/trends_avocado.csv")
+    fig = ff.create_dendrogram(apdf)
+    fig.update_layout(width=800, height=500)
+    fig.show()
+
+if __name__ == "__main__":
+    main()
