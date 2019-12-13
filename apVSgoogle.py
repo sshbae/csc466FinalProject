c4fe572cf2e8fbeff818277f2612a8c1afa3ee45
Sarah <sarahbae1998@gmail.com>
2019-12-12 22:50:38 -0800

    formatted output

--- apVSgoogle.py
+++ apVSgoogle.py
@@ -2,13 +2,72 @@ import scipy
 import plotly.figure_factory as ff
 import pandas as pd
 import numpy as np
+#from sklearn.cluster import DBSCAN
+from scipy.cluster import vq
+import matplotlib.pyplot as plt
+import json
+import avoPriceRegionsToStates
 
 def main():
-    apdf = pd.read_csv("./finalProj/avoPricesCategor.csv")
-    googdf = pd.read_csv("./finalProj/trends_avocado.csv")
-    fig = ff.create_dendrogram(apdf)
+    apdf = pd.read_csv("./finalProj/avoPricesNumerical.csv")
+    apRegions = apdf["region"]
+    for i in range(apRegions.size):
+        apRegions[i] = avoPriceRegionsToStates.REGION_TO_STATES[apRegions[i]]
+    apdf.drop("region", axis=1, inplace = True)
+
+    googdf = pd.read_csv("./finalProj/trends_avocado.csv",index_col = 'state')
+    googdf.fillna(0, inplace=True)
+    googdf = googdf.astype(np.float)
+    googRegions = np.frompyfunc(lambda x:x[3:],1,1)(list(googdf.index))
+
+#hclustering
+    #fig = ff.create_dendrogram(apdf,labels=regions.values)
+    #fig.update_layout(width=2500, height=800)
+    #fig.show()
+
+    fig = ff.create_dendrogram(googdf,labels=list(googdf.index))
     fig.update_layout(width=800, height=500)
     fig.show()
+
+    #numRegions = pd.unique(regions).size
+
+#db scan
+    #db = DBSCAN(eps=0.3, min_samples=numRegions/apdf.shape[0]).fit(newdf)
+    #dbPlot(db, newdf)
+
+#kmeans
+    ap_centroids, ap_clusters = vq.kmeans2(apdf, k=10, minit='points')
+    goog_centroids, goog_clusters = vq.kmeans2(googdf, k=10, minit='points')
+    apClusters = {}
+    googClusters = {}
+    for i in range(ap_clusters.size):
+        key = str(ap_clusters[i])
+        if key in apClusters:
+            apClusters[key].append(apRegions[i])
+        else:
+            apClusters[key] = [apRegions[i]]
+
+    for i in range(goog_clusters.size):
+        key = str(goog_clusters[i])
+        if key in googClusters:
+            googClusters[key].append(googRegions[i])
+        else:
+            googClusters[key] = [googRegions[i]]
+
+    print("Avocado Prices", end="")
+    for key in apClusters:
+        print(f"\nCluster {key}:")
+        temp = apClusters[key]
+        uniqueStates, countStates = np.unique(temp, return_counts=True)
+        count_sort_ind = np.argsort(-countStates)
+        uniques = uniqueStates[count_sort_ind]
+        print(uniques)
+
+    print("\nGoogle Searches")
+    #print(json.dumps(googClusters, indent=2))
+    for key in googClusters:
+        print(f"\nCluster {key}:")
+        print(googClusters[key])
 
 if __name__ == "__main__":
     main()
--- avoPriceRegionsToStates.py
+++ avoPriceRegionsToStates.py
@@ -1,14 +1,16 @@
 # excludes: GreatLakes, Midsouth, Northeast, NorthernNewEngland, Plains, SouthCentral, Southeast, TotalUS, West
 
-REGION_TO_STATES = {'Albany': 'US-CA', 'Atlanta': 'US-GA', 'BaltimoreWashington': 'US-MD', 'Boise': 'US-ID',
-                    'Boston': 'US-MA', 'BuffaloRochester': 'US-NY', 'California': 'US-CA', 'Charlotte': 'US-NC',
-                    'Chicago': 'US-IL', 'CincinnatiDayton': 'US-OH', 'Columbus': 'US-OH', 'DallasFtWorth': 'US-TX',
-                    'Denver': 'US-CO', 'Detroit': 'US-MI', 'GrandRapids': 'US-MI', 'HarrisburgScranton': 'US-PA',
-                    'HartfordSpringfield': 'US-CT', 'Houston': 'US-TX', 'Indianapolis': 'US-IN',
-                    'Jacksonville': 'US-FL', 'LasVegas': 'US-NV', 'LosAngeles': 'US-CA', 'Louisville': 'US-KY',
-                    'MiamiFtLauderdale': 'US-FL', 'Nashville': 'US-TN', 'NewOrleansMobile': 'US-LA', 'NewYork': 'US-NY',
-                    'Orlando': 'US-FL', 'Philadelphia': 'US-PA', 'PhoenixTucson': 'US-AZ', 'Pittsburgh': 'US-PA',
-                    'Portland': 'US-OR', 'RaleighGreensboro': 'US-NC', 'RichmondNorfolk': 'US-VA', 'Roanoke': 'US-VA',
-                    'Sacramento': 'US-CA', 'SanDiego': 'US-CA', 'SanFrancisco': 'US-CA', 'Seattle': 'US-WA',
-                    'SouthCarolina': 'US-SC', 'Spokane': 'US-WA', 'StLouis': 'US-MO', 'Syracuse': 'US-NY',
-                    'Tampa': 'US-FL', 'WestTexNewMexico': 'US-NM'}
+REGION_TO_STATES = {'Albany': 'CA', 'Atlanta': 'GA', 'BaltimoreWashington': 'MD', 'Boise': 'ID',
+                    'Boston': 'MA', 'BuffaloRochester': 'NY', 'California': 'CA', 'Charlotte': 'NC',
+                    'Chicago': 'IL', 'CincinnatiDayton': 'OH', 'Columbus': 'OH', 'DallasFtWorth': 'TX',
+                    'Denver': 'CO', 'Detroit': 'MI', 'GrandRapids': 'MI', 'HarrisburgScranton': 'PA',
+                    'HartfordSpringfield': 'CT', 'Houston': 'TX', 'Indianapolis': 'IN',
+                    'Jacksonville': 'FL', 'LasVegas': 'NV', 'LosAngeles': 'CA', 'Louisville': 'KY',
+                    'MiamiFtLauderdale': 'FL', 'Nashville': 'TN', 'NewOrleansMobile': 'LA', 'NewYork': 'NY',
+                    'Orlando': 'FL', 'Philadelphia': 'PA', 'PhoenixTucson': 'AZ', 'Pittsburgh': 'PA',
+                    'Portland': 'OR', 'RaleighGreensboro': 'NC', 'RichmondNorfolk': 'VA', 'Roanoke': 'VA',
+                    'Sacramento': 'CA', 'SanDiego': 'CA', 'SanFrancisco': 'CA', 'Seattle': 'WA',
+                    'SouthCarolina': 'SC', 'Spokane': 'WA', 'StLouis': 'MO', 'Syracuse': 'NY',
+                    'Tampa': 'FL', 'WestTexNewMexico': 'NM','GreatLakes':'MI', 'Midsouth':'TX', 'Northeast':'ME', 'NorthernNewEngland':'ME',
+                    'Plains':'NE', 'SouthCentral':'TX','Southeast':'FL','West':'CA'
+                    }
