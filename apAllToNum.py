e51b07f7cc7d77b175e3e4e2162bc7b59b37a5c7
Sarah <sarahbae1998@gmail.com>
2019-12-12 16:29:41 -0800

    ap all to num

--- apAllToNum.py
+++ apAllToNum.py
@@ -5,12 +5,14 @@ import numpy as np
 
 def main():
     apdf = pd.read_csv("./finalProj/avoPrices.csv")
+    apdf = apdf.drop(apdf.columns[0], axis = 1)
     apdf["Date"] = np.frompyfunc(lambda x:x[5:7],1,1)(apdf["Date"])
     
     apdf = apdf.rename(columns={"Date": "Month"})
+    apdf = apdf.groupby(["Month", "year", "region", "type"], as_index=False).sum()
     apdf = pd.get_dummies(apdf, columns = ["type"])
-    outfile = open("./finalProj/avoPricesMonths.csv", "w+")
-    apdf.to_csv(outfile)
+    outfile = open("./finalProj/avoPricesNumerical.csv", "w+")
+    apdf.to_csv(outfile, index=False)
 
 if __name__ == '__main__':
     main()
