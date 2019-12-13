5c4757a2ac9aa13e6131dac18209f87807b15558
wesley <wbenica@calpoly.edu>
2019-12-12 21:24:21 -0800

    region to state dictionary

---
+++ avoPriceRegionsToStates.py
@@ -0,0 +1,14 @@
+# excludes: GreatLakes, Midsouth, Northeast, NorthernNewEngland, Plains, SouthCentral, Southeast, TotalUS, West
+
+REGION_TO_STATES = {'Albany': 'US-CA', 'Atlanta': 'US-GA', 'BaltimoreWashington': 'US-MD', 'Boise': 'US-ID',
+                    'Boston': 'US-MA', 'BuffaloRochester': 'US-NY', 'California': 'US-CA', 'Charlotte': 'US-NC',
+                    'Chicago': 'US-IL', 'CincinnatiDayton': 'US-OH', 'Columbus': 'US-OH', 'DallasFtWorth': 'US-TX',
+                    'Denver': 'US-CO', 'Detroit': 'US-MI', 'GrandRapids': 'US-MI', 'HarrisburgScranton': 'US-PA',
+                    'HartfordSpringfield': 'US-CT', 'Houston': 'US-TX', 'Indianapolis': 'US-IN',
+                    'Jacksonville': 'US-FL', 'LasVegas': 'US-NV', 'LosAngeles': 'US-CA', 'Louisville': 'US-KY',
+                    'MiamiFtLauderdale': 'US-FL', 'Nashville': 'US-TN', 'NewOrleansMobile': 'US-LA', 'NewYork': 'US-NY',
+                    'Orlando': 'US-FL', 'Philadelphia': 'US-PA', 'PhoenixTucson': 'US-AZ', 'Pittsburgh': 'US-PA',
+                    'Portland': 'US-OR', 'RaleighGreensboro': 'US-NC', 'RichmondNorfolk': 'US-VA', 'Roanoke': 'US-VA',
+                    'Sacramento': 'US-CA', 'SanDiego': 'US-CA', 'SanFrancisco': 'US-CA', 'Seattle': 'US-WA',
+                    'SouthCarolina': 'US-SC', 'Spokane': 'US-WA', 'StLouis': 'US-MO', 'Syracuse': 'US-NY',
+                    'Tampa': 'US-FL', 'WestTexNewMexico': 'US-NM'}
